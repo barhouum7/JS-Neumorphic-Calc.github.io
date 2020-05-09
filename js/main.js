1cd0e28012841bc7766a16554df7fa6d60d09300
barhouum7 <bb.med2000@gmail.com>
2020-05-10 00:38:40 +0100

    Add JavaScript part

--- js/main.js
+++ js/main.js
@@ -6,3 +6,9 @@ document.querySelectorAll('.num__key').forEach(
         elmnt.onclick = () => input.value = input.value !== '0' ? input.value + elmnt.innerText : elmnt.innerText;
     }
 )
+
+const buffer = [];
+
+for (const opName of ['add', 'subtract', 'multiply', 'devide', 'percent']) {
+    document.querySelector(`.op__key[op=${opName}]`).onclick = opCallBack(opName);
+}
